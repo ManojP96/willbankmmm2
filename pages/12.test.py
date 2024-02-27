b72f5e1b3d7222f93a3fc2bf3794dbce59709d77
ManojP96 <129957999+ManojP96@users.noreply.github.com>
2024-02-27 11:40:35 +0530

    Update 12.test.py

--- pages/12.test.py
+++ pages/12.test.py
@@ -5,9 +5,9 @@ import streamlit as st
 count=0
 def test():
     global count
-    while count<1:
-        time.sleep(60*5)
+    while count<5:
+        time.sleep(60)
         count+=1
         st.write(count)
 
-st.button('test',on_click=test)
+st.button('test',on_click=test))
